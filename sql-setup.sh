4e9fbe719b1e44619cd3cd00ccc10aa74844a9c3
GilesFa <vsopk999@gmail.com>
2022-04-11 22:56:09 +0800

    Update sql-setup.sh

--- sql-setup.sh
+++ sql-setup.sh
@@ -15,7 +15,7 @@ else
 fi
 
 #------------------------安裝交互自動工具----------------------------#
-yum install tcl expect docker-ce git mysql ifconfig -y
+yum install tcl expect docker-ce git mysql net-tools -y
 
 
 #------------------------git clone-----------------------------------#
